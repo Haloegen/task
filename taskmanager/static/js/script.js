bcf3dbcc8df03a3197d8a62db40dbde4e60d5749
Haloegen <>
2023-11-24 16:45:46 +0000

    added add task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     // side nav installation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd, mm, yyyy,",
+        i18n: { done: "Select" },
+
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
